041407e5d2f5de69411843026dbcf9e69d6a3376
Rocco <grigoreandrey23@gmail.com>
2022-03-15 15:18:44 +0200

    added rectangle visualization

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
+
+const int WINDOW_WIDTH = 600;
+const int WINDOW_HEIGHT = 400;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML works!");
+
+    std::vector<sf::RectangleShape> rects;
+
+    for(int i = 0; i < 10; i++)
+    {
+        float height = (WINDOW_HEIGHT - 20) * ((i+1) / 10.f);
+        sf::RectangleShape temp({WINDOW_WIDTH / 10, height});
+        temp.setFillColor(sf::Color::White);
+        temp.setOutlineThickness(1);
+        temp.setOutlineColor(sf::Color::Black);
+        temp.setOrigin({WINDOW_WIDTH / 20, height});
+        temp.setPosition({WINDOW_WIDTH / 10.f * i + WINDOW_WIDTH / 20.f, WINDOW_HEIGHT});
+        rects.push_back(temp);
+    }
 
     while (window.isOpen())
     {
@@ -16,7 +33,10 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        for(int i = 0; i < 10; i++)
+        {
+            window.draw(rects[i]);
+        }
         window.display();
     }
 
