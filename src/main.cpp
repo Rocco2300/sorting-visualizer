7d306bf158a1ad53c9177a0bbc34891a43044e4d
Rocco <grigoreandrey23@gmail.com>
2022-03-17 16:03:30 +0200

    added colors to swapping

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,28 @@
 const int WINDOW_WIDTH = 600;
 const int WINDOW_HEIGHT = 400;
 
-void bubbleSort(std::vector<float>& vec)
+struct Element
+{
+    float height;
+    sf::Color color;
+};
+
+void bubbleSort(std::vector<Element>& vec)
 {
     for(size_t i = 0; i < vec.size()-1; i++)
     {
         for(size_t j = 0; j < vec.size()-i-1; j++)
         {
-            if(vec[j] > vec[j+1])
+            if(vec[j].height > vec[j+1].height)
             {
+                vec[j].color = sf::Color::Red;
                 std::swap(vec[j], vec[j+1]);
             }
-            std::this_thread::sleep_for(std::chrono::milliseconds(30));
+            else
+                vec[j].color = sf::Color::White;
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
         }
+        vec[vec.size()-i-1].color = sf::Color::White;
     }
 }
 
@@ -29,19 +39,21 @@ int main()
     srand(time(NULL));
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML works!");
 
-    std::vector<float> heights;
+    std::vector<Element> elems;
 
     int rectNo = 20;
-    heights.reserve(rectNo);
+    elems.reserve(rectNo);
     for(int i = 0; i < rectNo; i++)
     {
-        float height = (WINDOW_HEIGHT - 20) * ((i+1) / (float)rectNo);
-        heights.push_back(height);
+        Element el;
+        el.height = (WINDOW_HEIGHT - 20) * ((i+1) / (float)rectNo);
+        el.color = sf::Color::White;
+        elems.push_back(el);
     }
 
-    std::random_shuffle(heights.begin(), heights.end());
+    std::random_shuffle(elems.begin(), elems.end());
     // bubbleSort(heights);
-    std::thread th(bubbleSort, std::ref(heights));
+    std::thread th(bubbleSort, std::ref(elems));
 
     while (window.isOpen())
     {
@@ -55,9 +67,9 @@ int main()
         window.clear();
         for(int i = 0; i < rectNo; i++)
         {
-            sf::RectangleShape temp({WINDOW_WIDTH / 20, heights[i]});
-            temp.setFillColor(sf::Color::White);
-            temp.setOrigin({WINDOW_WIDTH / (float)rectNo, heights[i]});
+            sf::RectangleShape temp({WINDOW_WIDTH / 20, elems[i].height});
+            temp.setFillColor(elems[i].color);
+            temp.setOrigin({WINDOW_WIDTH / (float)rectNo, elems[i].height});
             temp.setPosition({WINDOW_WIDTH / (float)rectNo * i + WINDOW_WIDTH / rectNo, WINDOW_HEIGHT});
             window.draw(temp);
         }
