2680fe7dc50179a46261fe9da884d9eb8f7e33fc
Rocco <grigoreandrey23@gmail.com>
2022-03-17 15:36:25 +0200

    added bubble sort visualizer

--- src/main.cpp
+++ src/main.cpp
@@ -4,41 +4,23 @@
 #include <random>
 #include <ctime>
 #include <algorithm>
+#include <thread>
 
 const int WINDOW_WIDTH = 600;
 const int WINDOW_HEIGHT = 400;
 
-void swapShapes(sf::RectangleShape* shape1, sf::RectangleShape* shape2)
+void bubbleSort(std::vector<float>& vec)
 {
-    // auto width = shape1.getGlobalBounds().width;
-    // auto height1 = shape1.getGlobalBounds().height;
-    // auto height2 = shape2.getGlobalBounds().height;
-
-    // auto origin1 = shape1.getOrigin();
-    // auto origin2 = shape2.getOrigin();
-
-    // auto pos1 = shape1.getPosition();
-    // auto pos2 = shape2.getPosition();
-
-    // shape1.setSize({width, height2});
-    // shape1.setOrigin(origin2);
-    // shape1.setPosition(pos2);
-
-    // shape2.setSize({width, height1});
-    // shape2.setOrigin(origin1);
-    // shape2.setPosition(pos1);
-    auto temp = shape1;
-    shape1 = shape2;
-    shape2 = temp;
-}
-
-void shuffle(std::vector<sf::RectangleShape> shapes)
-{
-    for(size_t i = shapes.size()-1; i >= 1; i--)
+    for(size_t i = 0; i < vec.size()-1; i++)
     {
-        int j = rand() % i;
-        std::swap(shapes[i], shapes[j]);
-        // swapShapes(&shapes[i], &shapes[j]);
+        for(size_t j = 0; j < vec.size()-i-1; j++)
+        {
+            if(vec[j] > vec[j+1])
+            {
+                std::swap(vec[j], vec[j+1]);
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(30));
+        }
     }
 }
 
@@ -56,17 +38,10 @@ int main()
         float height = (WINDOW_HEIGHT - 20) * ((i+1) / (float)rectNo);
         heights.push_back(height);
     }
-    // for(int i = 0; i < rectNo; i++)
-    // {
-    //     float height = (WINDOW_HEIGHT - 20) * ((i+1) / (float)rectNo);
-    //     sf::RectangleShape temp({WINDOW_WIDTH / 20, height});
-    //     temp.setFillColor(sf::Color::White);
-    //     temp.setOrigin({WINDOW_WIDTH / (float)rectNo, height});
-    //     temp.setPosition({WINDOW_WIDTH / (float)rectNo * i + WINDOW_WIDTH / rectNo, WINDOW_HEIGHT});
-    //     rects.push_back(temp);
-    // }
 
     std::random_shuffle(heights.begin(), heights.end());
+    // bubbleSort(heights);
+    std::thread th(bubbleSort, std::ref(heights));
 
     while (window.isOpen())
     {
@@ -88,6 +63,6 @@ int main()
         }
         window.display();
     }
-
+    th.join();
     return 0;
 }
