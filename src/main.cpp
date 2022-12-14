ae87dc9a6eb4441110c572417910cb667e38a6f2
Grigore Eugen-Andrei <grigoreandrey23@gmail.com>
2022-12-14 10:11:31 +0200

    add error ignore to stop console spam

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 
 int main()
 {
+    // Suppres false Failed to set DirectInput device axis mode: 1 error 
+    sf::err().rdbuf(nullptr);
+
     Program program;
     program.update();
     return 0;
